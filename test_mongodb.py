233c4e838cdf690d5f794e8a5809d91494b08f01
Chirag Verma <chirag.yep@gmail.com>
2025-02-18 12:27:38 +0530

    MongoDB connection established

---
+++ test_mongodb.py
@@ -0,0 +1,14 @@
+# Code to check whether there is a successful connection with mongodb or not
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://chiragyep:Chirag24@cluster0.riftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)

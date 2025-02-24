d4bd62566e7f6df3b6642d72fd0b9681a7d533fc
Chirag Verma <chirag.yep@gmail.com>
2025-02-24 23:58:04 +0530

    data transformation constants added

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -37,4 +37,21 @@ DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
+# DATA Transformation
+"""
+Data Transformation related constants starts with DATA_VALIDATION VAR NAME
+"""
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DIR_NAME: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_NAME: str = "transformed_object"
+
+# K-NN Imputer for Missing Values {replace NaN Values}
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3, 
+    "weights": "uniform"
+}
+
+
 

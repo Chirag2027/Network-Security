d770829c3a4e4ac8c42812faea7b4c228d127988
Chirag Verma <chirag.yep@gmail.com>
2025-02-25 01:53:48 +0530

    successfully tested data transformation stage

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Just testing whether data ingestion, validation is working fine or not
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -22,7 +24,14 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation completed")
         print(data_validation_artifact)
-        
+
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data Transformation Started")
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Initiate the Data Transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation completed")
+        print(data_transformation_artifact)
         
 
     except Exception as e:

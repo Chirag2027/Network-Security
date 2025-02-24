689029d4eb5932aadfaeeee675360a4ace419d2a
Chirag Verma <chirag.yep@gmail.com>
2025-02-25 01:46:25 +0530

    Implemented 2 generic functions for data transformation stage

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -5,6 +5,7 @@ import os, sys
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 import pickle
+import numpy as np
 # import dill
 
 # Function to read yaml file
@@ -28,3 +29,32 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
 
     except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+# Functions for data transformation : to save the numpy array and to save the preprocessor object
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file,
+    file_path -> location of file to be saved
+    array: np.array -> data to be saved
+    """
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+# Saving the pickle file 
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered into the save_object method of main.utils ")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of main.util")
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)

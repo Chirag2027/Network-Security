ea624aca6f78b6d554a0d492f6f2ac9879a1b012
Chirag Verma <chirag.yep@gmail.com>
2025-02-25 16:20:25 +0530

    Final testing of model trainer

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Just testing whether data ingestion, validation is working fine or not
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import DataTransformationConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -32,6 +33,13 @@ if __name__ == "__main__":
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Data Transformation completed")
         print(data_transformation_artifact)
+
+        logging.info("Model Training Started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        logging.info("Model training initiated")
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Trainer Artifact Created Successfully")
         
 
     except Exception as e:

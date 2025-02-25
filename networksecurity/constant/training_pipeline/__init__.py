f41acaa7cfec7333da5be8672770b50edf8b1862
Chirag Verma <chirag.yep@gmail.com>
2025-02-25 16:18:32 +0530

    constants related to model trainer added

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -13,6 +13,8 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
 
 """
@@ -39,7 +41,7 @@ DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 # DATA Transformation
 """
-Data Transformation related constants starts with DATA_VALIDATION VAR NAME
+Data Transformation related constants starts with DATA_TRANSFORMATION VAR NAME
 """
 
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
@@ -55,5 +57,14 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
 
 PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
 
+# MODEL TRAINING
+"""
+Model Trainer related constants starts with MODEL_TRAINERVAR NAME
+"""
 
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
 

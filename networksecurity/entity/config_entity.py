bdc3f545f2a54c700f22907dc38ce31169285bde
Chirag Verma <chirag.yep@gmail.com>
2025-02-25 16:19:17 +0530

    model trainer config setup and model trainer artifact and classification metric artifact done

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -72,7 +72,22 @@ class DataTransformationConfig:
 
         self.transformed_object_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_NAME, training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
 
-        
+# Model Training & Evaluation 
+# MODEL TRAINER CONFIG
+class ModelTrainerConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.training_pipeline_config = training_pipeline_config
+        self.model_trainer_dir: str = os.path.join(self.training_pipeline_config.artifact_dir, 
+        training_pipeline.MODEL_TRAINER_DIR_NAME)
+
+        self.trained_model_file_path: str = os.path.join(self.model_trainer_dir, training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR, training_pipeline.MODEL_FILE_NAME)
+
+        self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD
+
+
+
+
 
 
 

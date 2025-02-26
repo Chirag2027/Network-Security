e1ff927d4f302715690401188ca3f6fbce74715d
Chirag Verma <chirag.yep@gmail.com>
2025-02-26 23:30:44 +0530

    Model Pusher implementation done successfully in local folder:

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -101,6 +101,9 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr, )
             save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_object, )
 
+            # saving the preprocessor file
+            save_object("final_model/preprocessor.pkl", preprocessor_object, )
+
             # preparing our artifacts -> O/P which will be returned after Data Transformation stage
             data_transformation_artifact = DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
